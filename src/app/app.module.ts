7fd70919dcc9742e014f208a491044caf5a86098
merlinpratheesh <merlinpratheesh@gmail.com>
2021-04-30 18:42:45 +0530

    createObjectURL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AudioRecordingService } from './audio-recording.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VideoRecordingService } from './video-recording.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AudioRecordingService,VideoRecordingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
